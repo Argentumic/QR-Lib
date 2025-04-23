d5932b35bc8d640349bcdac2598aa1b10bde65ac
Argentumic <artemtarabasyt@gmail.com>
2025-04-23 21:20:46 +0300

    corrected version mistake

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.qrlib"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
