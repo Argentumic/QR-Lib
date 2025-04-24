92ce7225bb651263a582640af2c57deb37a626b8
Argentumic <artemtarabasyt@gmail.com>
2025-04-24 20:18:18 +0300

    Doing the essentials, added button and dialog

--- app/src/main/java/com/example/qrlib/MainActivity.kt
+++ app/src/main/java/com/example/qrlib/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.qrlib
 
+import android.R.attr.onClick
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.media.Image
@@ -13,11 +14,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,7 +44,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.window.Dialog
 import com.example.qrlib.ui.theme.QRБібліотекаTheme
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
@@ -41,13 +63,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QRБібліотекаTheme {
-                Box(modifier =
-                    Modifier
-                    .background(Color(red = 0, green = 0, blue = 0))
-                    .fillMaxSize())
-                {
-                    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                        SimpleOutlinedTextFieldSample()
+                var showDialog by remember { mutableStateOf(false) }
+
+                Box(
+                    modifier = Modifier
+                        .background(Color(red = 0, green = 0, blue = 0))
+                        .fillMaxSize()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.End,
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+                        AddNewQRButton(onClick = { showDialog = true })
+
+                        if (showDialog) {
+                            DialogWithImage(
+                                onDismissRequest = { showDialog = false },
+                                onConfirmation = { showDialog = false }
+                            )
+                        }
                     }
                 }
             }
@@ -76,9 +113,11 @@ fun QrCodeView(url: String) {
 fun SimpleOutlinedTextFieldSample() {
     var text by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+    Column(modifier = Modifier.fillMaxWidth().heightIn(min = 50.dp, max = 300.dp).padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
         if (text.isNotBlank()) {
             QrCodeView(url = text)
+        }else{
+            QrCodeView(url = " ")
         }
 
         OutlinedTextField(
@@ -86,5 +125,58 @@ fun SimpleOutlinedTextFieldSample() {
             onValueChange = { text = it },
             label = { Text("Label") }
         )
+    }
+}
+
+@Composable
+fun AddNewQRButton(onClick: () -> Unit) {
+    OutlinedButton(onClick = onClick, modifier = Modifier.size(72.dp)) {
+        Icon(
+            Icons.Rounded.Add,
+            contentDescription = "Add QR Code",
+            tint = Color.White,
+            modifier = Modifier.size(36.dp)
+        )
+    }
+}
+
+@Composable
+fun DialogWithImage(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                SimpleOutlinedTextFieldSample()
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = onDismissRequest,
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Dismiss")
+                    }
+                    TextButton(
+                        onClick = onConfirmation,
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            }
+        }
     }
 }
